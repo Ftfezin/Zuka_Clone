0e4b8d0e999b247e6194831b089f5fd05a4103e0
Fezin <126676831+Ftfezin@users.noreply.github.com>
2024-02-05 16:01:42 +0530

    script.js

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const sr = ScrollReveal ({
     reset: true
 });
 
-sr.reveal('.home-content img', {delay:1, origin: "top"});
-sr.reveal('.home-content h1', {delay:1, origin: "left"});
-sr.reveal('.shop-btn', {delay:22, origin: "left"});
+// sr.reveal('.home-content img', {delay:1, origin: "top"});
+// sr.reveal('.home-content h1', {delay:1, origin: "left"});
+// sr.reveal('.shop-btn', {delay:22, origin: "left"});
 
-sr.reveal('.home2-content h2', {delay:1, origin: "right"});
-sr.reveal('.home2-content p', {delay:1, origin: "right"});
+// sr.reveal('.home2-content h2', {delay:1, origin: "right"});
+// sr.reveal('.home2-content p', {delay:1, origin: "right"});
 
-sr.reveal('.home3-content h2', {delay:1, origin: "left"});
-sr.reveal('.home3-content p', {delay:1, origin: "left"});
+// sr.reveal('.home3-content h2', {delay:1, origin: "left"});
+// sr.reveal('.home3-content p', {delay:1, origin: "left"});
 
-sr.reveal('.home4-content h1', {delay:1, origin: "left"})
-sr.reveal('.home4-content img', {delay:1, origin: "top"});
+// sr.reveal('.home4-content h1', {delay:1, origin: "left"})
+// sr.reveal('.home4-content img', {delay:1, origin: "top"});
